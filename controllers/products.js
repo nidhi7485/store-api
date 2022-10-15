bfa959436eb6ab92d2e5a321d33ee1219a07d584
nidhi <nidhi7485@gmail.com>
2022-10-16 00:49:55 +0530

    "fetch from api"

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,31 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async error')
-  res.status(200).json({ msg: 'product testing route' })
+  const product = await Product.find({ name: 'wooden table' })
+  res.status(200).json({ product, nbHits: product.length })
 }
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'product route' })
+  const search = 'a'
+
+  const { featured, company, name } = req.query
+  const queryObject = {}
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = name
+  }
+  const product = await Product.find({
+    name: {
+      $regex: search,
+      $options: 'i',
+    },
+  })
+  res.status(200).json({ product, nbHits: product.length })
 }
 
 module.exports = {
