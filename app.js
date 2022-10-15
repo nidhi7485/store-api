68ce37a27b21ae639ccb9b5f56fe0c2dd0dda073
nidhi <nidhi7485@gmail.com>
2022-10-15 17:12:35 +0530

    populate DB

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 require('dotenv').config()
-
+require('express-async-errors')
 const express = require('express')
 const app = express()
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const connectDB = require('./db/connect')
+
+// route
+const productRouter = require('./routes/products')
+
 // middleware
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('<h1>Store Api</h1><a href="/api/v1/products">products route</a>')
 })
-
+app.use('/api/v1/products', productRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
+
 const port = process.env.PORT || 3000
 
 // app.listen(port, console.log(`server is listening on port ${port}...`))
